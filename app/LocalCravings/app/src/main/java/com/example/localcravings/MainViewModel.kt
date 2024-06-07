5b6cc392d198e2634f690845383af426483016dc
Jeffrey Sheets <84060666+Jeffrey-Sheets@users.noreply.github.com>
2024-06-07 14:28:58 -0400

    Add files via upload

---
+++ app/LocalCravings/app/src/main/java/com/example/localcravings/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.localcravings
+
+import android.os.Build
+import android.os.LocaleList
+import android.os.Parcelable
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
+import java.util.Timer
+import java.util.TimerTask
+
+@Parcelize
+data class Item(
+    val name: String = "",
+    val description: String = "",
+    val cost: Int = 0,
+    var bought: Boolean = false
+): Parcelable
+class MainViewModel {
+}
